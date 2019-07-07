48431d3f6d5b87b3582438fba9ca2c64cbd7328a
volanja <volaaanja@gmail.com>
2019-07-07 23:04:39 +0900

    update ansible_spec v0.3

--- Rakefile
+++ Rakefile
@@ -6,13 +6,20 @@ require 'ansible_spec'
 properties = AnsibleSpec.get_properties
 # {"name"=>"Ansible-Sample-TDD", "hosts"=>["192.168.0.103","192.168.0.103"], "user"=>"root", "roles"=>["nginx", "mariadb"]}
 # {"name"=>"Ansible-Sample-TDD", "hosts"=>[{"name" => "192.168.0.103:22","uri"=>"192.168.0.103","port"=>22, "private_key"=> "~/.ssh/id_rsa"}], "user"=>"root", "roles"=>["nginx", "mariadb"]}
+cfg = AnsibleSpec::AnsibleCfg.new
+
+desc "Run serverspec to all test"
+task :all => "serverspec:all"
 
 namespace :serverspec do
+  properties = properties.compact.reject{|e| e["hosts"].length == 0}
+  task :all => properties.map {|v| 'serverspec:' + v["name"] }
   properties.each_with_index.map do |property, index|
     property["hosts"].each do |host|
       desc "Run serverspec for #{property["name"]}"
       RSpec::Core::RakeTask.new(property["name"].to_sym) do |t|
         puts "Run serverspec for #{property["name"]} to #{host}"
+        ENV['TARGET_HOSTS'] = host["hosts"]
         ENV['TARGET_HOST'] = host["uri"]
         ENV['TARGET_PORT'] = host["port"].to_s
         ENV['TARGET_GROUP_INDEX'] = index.to_s
@@ -22,13 +29,20 @@ namespace :serverspec do
         else
           ENV['TARGET_USER'] = property["user"]
         end
+        ENV['TARGET_PASSWORD'] = host["pass"]
+        ENV['TARGET_CONNECTION'] = host["connection"]
 
         roles = property["roles"]
         for role in property["roles"]
-          deps = AnsibleSpec.load_dependencies(role)
-          roles += deps
+          for rolepath in cfg.roles_path
+            deps = AnsibleSpec.load_dependencies(role, rolepath)
+            if deps != []
+              roles += deps
+              break
+            end
+          end
         end
-        t.pattern = 'roles/{' + roles.join(',') + '}/spec/*_spec.rb'
+        t.pattern = '{' + cfg.roles_path.join(',') + '}/{' + roles.join(',') + '}/spec/*_spec.rb'
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,47 +1,100 @@
 require 'serverspec'
 require 'net/ssh'
 require 'ansible_spec'
-
-set :backend, :ssh
-
-if ENV['ASK_SUDO_PASSWORD']
-  begin
-    require 'highline/import'
-  rescue LoadError
-    fail "highline is not available. Try installing it."
-  end
-  set :sudo_password, ask("Enter sudo password: ") { |q| q.echo = false }
-else
-  set :sudo_password, ENV['SUDO_PASSWORD']
-end
-
-host = ENV['TARGET_HOST']
-
-options = Net::SSH::Config.for(host)
-
-options[:user] ||= ENV['TARGET_USER']
-options[:port] ||= ENV['TARGET_PORT']
-options[:keys] ||= ENV['TARGET_PRIVATE_KEY']
-
-set :host,        options[:host_name] || host
-set :ssh_options, options
-
-# Disable sudo
-# set :disable_sudo, true
-
-
-# Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
-
-# Set PATH
-# set :path, '/sbin:/usr/local/sbin:$PATH'
+require 'winrm'
 
 #
 # Set ansible variables to serverspec property
 #
+host = ENV['TARGET_HOST']
+hosts = ENV["TARGET_HOSTS"]
 
 group_idx = ENV['TARGET_GROUP_INDEX'].to_i
-
-vars = AnsibleSpec.get_variables(host, group_idx)
-
+vars = AnsibleSpec.get_variables(host, group_idx,hosts)
+ssh_config_file = AnsibleSpec.get_ssh_config_file
 set_property vars
+
+connection = ENV['TARGET_CONNECTION']
+
+case connection
+when 'ssh'
+#
+# OS type: UN*X
+#
+  set :backend, :ssh
+
+  # Ansible use `BECOME`, But Serverspec use `SUDO`.
+  if ENV['ASK_BECOME_PASSWORD']
+    begin
+      require 'highline/import'
+    rescue LoadError
+      fail "highline is not available. Try installing it."
+    end
+    set :sudo_password, ask("Enter become password: ") { |q| q.echo = false }
+  else
+    set :sudo_password, ENV['BECOME_PASSWORD']
+  end
+
+  options = Net::SSH::Config.for(host)
+
+  options[:user] = ENV['TARGET_USER'] || options[:user]
+  options[:port] = ENV['TARGET_PORT'] || options[:port]
+  options[:keys] = ENV['TARGET_PRIVATE_KEY'] || options[:keys]
+
+  if ssh_config_file
+    from_config_file = Net::SSH::Config.for(host,files=[ssh_config_file])
+    options.merge!(from_config_file)
+  end
+
+  set :host,        options[:host_name] || host
+  set :ssh_options, options
+
+  # Disable become (Serverspec use sudo)
+  # set :disable_sudo, true
+
+
+  # Set environment variables
+  # set :env, :LANG => 'C', :LC_MESSAGES => 'C'
+
+  # Set PATH
+  # set :path, '/sbin:/usr/local/sbin:$PATH'
+when 'winrm'
+#
+# OS type: Windows
+#
+  set :backend, :winrm
+  set :os, :family => 'windows'
+
+  user = ENV['TARGET_USER']
+  port = ENV['TARGET_PORT']
+  pass = ENV['TARGET_PASSWORD']
+
+  if user.nil?
+    begin
+      require 'highline/import'
+    rescue LoadError
+      fail "highline is not available. Try installing it."
+    end
+    user = ask("\nEnter #{host}'s login user: ") { |q| q.echo = true }
+  end
+  if pass.nil?
+    begin
+      require 'highline/import'
+    rescue LoadError
+      fail "highline is not available. Try installing it."
+    end
+    pass = ask("\nEnter #{user}@#{host}'s login password: ") { |q| q.echo = false }
+  end
+
+  endpoint = "http://#{host}:#{port}/wsman"
+
+  winrm = ::WinRM::WinRMWebService.new(endpoint, :ssl, :user => user, :pass => pass, :basic_auth_only => true)
+  winrm.set_timeout 300 # 5 minutes max timeout for any operation
+  Specinfra.configuration.winrm = winrm
+
+when 'local'
+#
+# local connection
+#
+    set :backend, :exec
+end
